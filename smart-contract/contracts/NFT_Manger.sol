1d45a44ca3cd564fc5505f899f3d43d0a4abe98b
Sajad-Salehi <SajadSolidity@gmail.com>
2023-02-27 21:31:09 +0200

    Overrided Mint function

--- smart-contract/contracts/NFT_Manger.sol
+++ smart-contract/contracts/NFT_Manger.sol
@@ -4,32 +4,32 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-
-contract NFT_Manger is ERC721URIStorage {
+contract NFTManager is ERC721URIStorage {
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenId;
+    // Maps user addresses to their token IDs
     mapping (address => uint256) public AddressToID;
-    
 
-    constructor() ERC721("Arweave DI DApp", "DI DApp") {
 
-    }
+    constructor() ERC721("Arweave DI DApp", "DI DApp") {}
 
-    function mint_nft(string memory tokenURI) public  {
-        
+
+    // Mint a new NFT and assign it to the caller
+    function mintNFT(string memory tokenURI) public {
+
         uint256 newItemId = _tokenId.current();
 
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
         AddressToID[msg.sender] = newItemId;
-
         _tokenId.increment();
     }
 
-    
-    function getTokenID() public view returns(uint256){
 
+    // Get the ID of the most recently minted token
+    function getTokenID() public view returns (uint256) {
+        
         uint256 tokenID = _tokenId.current() - 1;
         return tokenID;
     }
