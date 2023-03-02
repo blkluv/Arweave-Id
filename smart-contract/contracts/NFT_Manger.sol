50d76d9328d327177b084ded26911da738547c74
Sajad-Salehi <SajadSolidity@gmail.com>
2023-03-02 16:41:16 +0200

    Fixed Auth Method

--- smart-contract/contracts/NFT_Manger.sol
+++ smart-contract/contracts/NFT_Manger.sol
@@ -9,21 +9,36 @@ contract NFTManager is ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenId;
     // Maps user addresses to their token IDs
-    mapping (address => uint256) public AddressToID;
+    mapping (address => uint256) public addressToId;
 
 
     constructor() ERC721("Arweave DI DApp", "DI DApp") {}
 
 
     // Mint a new NFT and assign it to the caller
-    function mintNFT(string memory tokenURI) public {
+    function mintNFT(string memory tokenURI, uint256 ArweaveFee) public payable {
 
+        require(msg.value >= ArweaveFee, "The message value is under than Arweave Fee amount");
         uint256 newItemId = _tokenId.current();
 
         _mint(msg.sender, newItemId);
         _setTokenURI(newItemId, tokenURI);
-        AddressToID[msg.sender] = newItemId;
+        addressToId[msg.sender] = newItemId;
         _tokenId.increment();
+    }
+
+    function authenticate(address _owner) public view returns(bool) {
+
+        uint256 token_id = addressToId[_owner];
+        address token_owner = IERC721(this).ownerOf(token_id);
+
+        if (token_owner == _owner) {
+            return true;
+        }
+
+        else{
+            return false;
+        }
     }
 
 
