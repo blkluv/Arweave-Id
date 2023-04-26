00d1c348f8052f5f9cb662feebe5e27252990146
Sajad Salehi <sajadsolidity@gmail.com>
2023-04-26 16:54:15 +0330

    Update ArweaveUploader.ts

--- src/pages/api/ArweaveUploader.ts
+++ src/pages/api/ArweaveUploader.ts
@@ -8,7 +8,31 @@ const bundlr = new Bundlr("https://devnet.bundlr.network","matic", privateKey, {
 
 async function ArweaveUploader(req: NextApiRequest, res: NextApiResponse) {
   try {
+    
     res.status(200).json({ walletAddress: bundlr.address });
+    const stats = fs.statSync(file);
+    const fileSize = stats.size;
+    const price = await bundlr.getPrice(fileSize);
+
+    const response = await bundlr.fund(price);
+    console.log(`Funding successful txID=${response.id} amount funded=${response.quantity}`);
+    
+    const fileData = fs.readFileSync(file);
+    const fileBuffer = Buffer.from(fileData);
+    const fileTransactionId = await bundlr.upload(fileBuffer);
+
+    const metadata = {
+        name: name,
+        description: description,
+        file: `https://arweave.net/${fileTransactionId}`
+    };
+    
+    const jsonData = JSON.stringify(metadata);
+    const jsonBuffer = Buffer.from(jsonData);
+    const jsonTransactionId = await bundlr.upload(jsonBuffer);
+
+
+    
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Something went wrong' });
