6624e6a87646b3840de0303f77245d722958abc8
Sajad-dev <sajadsolidity@gmail.com>
2023-07-07 22:11:26 +0330

    init commit **

--- smart-contract/scripts/deploy.py
+++
@@ -1,9 +0,0 @@
-from brownie import NFT_Manager, accounts
-
-
-
-def main():
-
-    account = accounts.load('dev1')
-    contract = NFT_Manager.deploy({"from": account}, publish_source=False)
-    print(f"NFT Manager contract deplyed at address: {contract}")
